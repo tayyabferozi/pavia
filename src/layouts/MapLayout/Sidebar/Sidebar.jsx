64316075df8d3d201cf1925282c4ea8b67537db9
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-10-09 21:48:29 +0500

    adds responsive design of map page

--- src/layouts/MapLayout/Sidebar/Sidebar.jsx
+++ src/layouts/MapLayout/Sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faClose, faBars } from "@fortawesome/free-solid-svg-icons";
 
 import GridContainer from "../../../components/GridContainer";
 import Button from "../../../components/Button";
 import Input from "../../../components/Input/Input";
 import Switch from "../../../components/Switch";
+import clsx from "clsx";
 
 const tabs = ["GENERAL", "MY PROFILE", "MY LAND"];
 
@@ -15,6 +16,11 @@ const Sidebar = () => {
     y: 0,
     address: "",
   });
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsSideBarOpen((prevState) => !prevState);
+  };
 
   const inputChangeHandler = (e) => {
     const { name, value } = e.target;
@@ -23,105 +29,114 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="sidebar">
-      <div className="d-flex align-items-center ps-3 pt-2">
-        <h4 className="">MAP</h4>
-        <div className="ps-3">v3.1.2</div>
+    <>
+      <div className="menu" onClick={toggleNavbar}>
+        <FontAwesomeIcon color="#fff" fontSize={20} icon={faBars} />
       </div>
-      <div className="tabs-head">
-        {tabs.map((el, idx) => {
-          return (
-            <Button
-              className="tab-item"
-              key={"tab-" + idx}
-              disabled={idx !== 0}
-            >
-              {el}
-            </Button>
-          );
-        })}
-      </div>
-
-      <div className="tab-content">
-        <div>
-          <Button lightGrey>RESET ZOOM</Button>
-          <div className="pt-1">
-            <Button className="mt-2" lightGrey>
-              CLEAR HIGHLIGHTS
-            </Button>
+      <div className={clsx("sidebar", isSideBarOpen && "open")}>
+        <div className="d-flex align-items-center ps-3 pt-2 p-relative">
+          <div className="close" onClick={toggleNavbar}>
+            <FontAwesomeIcon icon={faClose} />
           </div>
+          <h4 className="">MAP</h4>
+          <div className="ps-3">v3.1.2</div>
+        </div>
+        <div className="tabs-head">
+          {tabs.map((el, idx) => {
+            return (
+              <Button
+                className="tab-item"
+                key={"tab-" + idx}
+                disabled={idx !== 0}
+              >
+                {el}
+              </Button>
+            );
+          })}
         </div>
 
-        <div className="mt-5">
-          <Switch />
-        </div>
-
-        <div className="mt-4">
-          <div>GO TO COORDINATE</div>
-
-          <div className="pt-1">
-            <div className="mt-2">
-              <GridContainer className="pt-1" rowClassName="gx-2">
-                <div className="col-lg-6">
-                  <Input
-                    name="x"
-                    value={formState.x}
-                    onChange={inputChangeHandler}
-                  />
-                </div>
-                <div className="col-lg-6">
-                  <Input
-                    name="y"
-                    value={formState.y}
-                    onChange={inputChangeHandler}
-                  />
-                </div>
-              </GridContainer>
+        <div className="tab-content">
+          <div>
+            <Button lightGrey>RESET ZOOM</Button>
+            <div className="pt-1">
+              <Button className="mt-2" lightGrey>
+                CLEAR HIGHLIGHTS
+              </Button>
             </div>
           </div>
-          <div className="pt-2">
-            <Button className="mt-2" lightGrey>
-              GO TO COORDINATE
-            </Button>
-          </div>
-        </div>
 
-        <div className="mt-4">
-          <div>
-            FIND BY ADDRESS <FontAwesomeIcon className="ms-2" icon={faSearch} />
+          <div className="mt-5">
+            <Switch />
           </div>
 
-          <div className="pt-1">
-            <div className="mt-2">
-              <div className="pt-1">
-                <Input
-                  name="address"
-                  value={formState.address}
-                  onChange={inputChangeHandler}
-                  placeholder="Value"
-                />
+          <div className="mt-4">
+            <div>GO TO COORDINATE</div>
+
+            <div className="pt-1">
+              <div className="mt-2">
+                <GridContainer className="pt-1" rowClassName="gx-2">
+                  <div className="col-6">
+                    <Input
+                      name="x"
+                      value={formState.x}
+                      onChange={inputChangeHandler}
+                    />
+                  </div>
+                  <div className="col-6">
+                    <Input
+                      name="y"
+                      value={formState.y}
+                      onChange={inputChangeHandler}
+                    />
+                  </div>
+                </GridContainer>
               </div>
             </div>
+            <div className="pt-2">
+              <Button className="mt-2" lightGrey>
+                GO TO COORDINATE
+              </Button>
+            </div>
           </div>
-          <div className="pt-2">
-            <Button className="mt-2" lightGrey>
-              HIGHEST LAND
-            </Button>
+
+          <div className="mt-4">
+            <div>
+              FIND BY ADDRESS{" "}
+              <FontAwesomeIcon className="ms-2" icon={faSearch} />
+            </div>
+
+            <div className="pt-1">
+              <div className="mt-2">
+                <div className="pt-1">
+                  <Input
+                    name="address"
+                    value={formState.address}
+                    onChange={inputChangeHandler}
+                    placeholder="Value"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="pt-2">
+              <Button className="mt-2" lightGrey>
+                HIGHEST LAND
+              </Button>
+            </div>
           </div>
-        </div>
 
-        <div className="mt-5">
-          <div>CURSOR COORDINATES</div>
+          <div className="mt-5">
+            <div>CURSOR COORDINATES</div>
 
-          <div className="cursor-coordinates bg-light-gray rounded-lg mt-3">
-            <div>SPACE</div>
-            <h4 className="mt-1">(-321, -96)</h4>
+            <div className="cursor-coordinates bg-light-gray rounded-lg mt-3">
+              <div>SPACE</div>
+              <h4 className="mt-1">(-321, -96)</h4>
 
-            <img src="/assets/imgs/space.png" className="w-100" alt="space" />
+              <img src="/assets/imgs/space.png" className="w-100" alt="space" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
